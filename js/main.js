7b211ff70cdcd1270b4236077c9b93e336931539
Alexander Derevyanko <74137399+derevyankoolx@users.noreply.github.com>
2021-01-10 00:41:34 +0300

    Productly-project

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slick-slider').slick({
+        infinite: true,
+        slideToShow: 3,
+        slidetoScroll: 1,
+        arrows: false,
+        autoplaySpeed: 2000,
+    });
+
+  });
